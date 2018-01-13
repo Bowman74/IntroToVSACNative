1f6b99090c26eb8d07cfb39d0337820efc33c44c
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2018-01-13 11:45:58 -0500

    Add crash reporting and analytics to ios project.

--- src/iOS/VSACNative/VSACNative/AppDelegate.swift
+++ src/iOS/VSACNative/VSACNative/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        MSAppCenter.start("5797ff79-9b46-4df2-86ef-2ce5c6e82371", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self
+            ])
+        
         return true
     }
 
